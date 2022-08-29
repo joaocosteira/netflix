3a737a5caba18c5ba11e78e5eab6240f87feab8b
Costeira <joaocurveira13@gmail.com>
2022-08-29 13:21:14 +0100

    finished user page

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -19,7 +19,33 @@ const ProfileScreen =  () => {
                     <div className='profileScreen__details'>
                         <h2>{user.email}</h2>
                         <div className='profileScreen__plans'>
-                            <h3>Plans</h3>
+                            <h3>Plans (Current Plan: Premium)</h3>
+
+                            <div className='profileScreen__plansRow'>
+                                <div>
+                                    <p>Netflix Standard</p>
+                                    <p>1080p</p>
+                                </div>
+                                <button>Subscribe</button>
+                            </div>
+
+                            <div className='profileScreen__plansRow'>
+                                <div>
+                                    <p>Netflix Basic</p>
+                                    <p>720p</p>
+                                </div>
+                                <button>Subscribe</button>
+                            </div>
+
+                            <div className='profileScreen__plansRow planSelected'>
+                                <div>
+                                    <p>Netflix Premium</p>
+                                    <p>4k + HRD</p>
+                                </div>
+                                <button>Current Package</button>
+                            </div>
+
+
                             <button 
                                 className='profileScreen__signOut'
                                 onClick={() =>{ signOut(auth) }}
